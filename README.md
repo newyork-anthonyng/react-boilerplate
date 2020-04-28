537dd98fbaef8fd71b01a558091bbc04de5fbdfe
Anthony Ng <newyork.anthonyng@gmail.com>
2020-04-28 19:12:42 -0400

    add boilerplate

---
+++ README.md
@@ -0,0 +1,9 @@
+# Getting started
+1. Clone this repository
+```bash
+$ git clone https://github.com/newyork-anthonyng/react-boilerplate.git <YOUR_PROJECT_NAME>
+```
+2. Start the application in watch mode
+```bash
+$ npm start
+```
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/app.js",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+};
