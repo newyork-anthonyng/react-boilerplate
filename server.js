ef684899f7651e88f8c91b1c37dde82a97e98235
Anthony Ng <newyork.anthonyng@gmail.com>
2020-05-14 08:40:58 -0400

    use correct heroku variable for port

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(`/`, express.static(path.resolve(__dirname, "dist")));
 
-const listener = app.listen(process.env.HOST || 3000, () => {
+const listener = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server listening on ${listener.address().port}`);
 });
