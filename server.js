76ea2ed4901605e8ef94c24b206ff30f14dab28b
Anthony Ng <newyork.anthonyng@gmail.com>
2020-05-27 18:13:04 -0400

    wip

--- server.js
+++ server.js
@@ -1,8 +1,55 @@
 const path = require("path");
 const express = require("express");
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const mongoose = require('mongoose');
+const errorHandler = require('errorhandler');
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 const app = express();
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(session({
+  secret: 'my-tutorial',
+  cookie: { maxAge: 60000 },
+  resave: false,
+  saveUninitialized: false
+}));
+
+if (!isProduction) {
+  app.use(errorHandler);
+}
+
+mongoose.connect('mongodb://localhost/passport-tutorial');
+mongoose.set('debug', true);
+
+if (!isProduction) {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err
+      }
+    });
+  })
+}
 
 app.use(`/`, express.static(path.resolve(__dirname, "dist")));
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {}
+    }
+  })
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server listening on ${listener.address().port}`);
