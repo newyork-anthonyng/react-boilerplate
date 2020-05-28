4c495f5fae347043850362c088e6cdcdcd9840d4
Anthony Ng <newyork.anthonyng@gmail.com>
2020-05-27 20:28:46 -0400

    feat: add express boilerplate (#2)

--- server.js
+++ server.js
@@ -1,8 +1,59 @@
-const path = require("path");
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const mongoose = require("mongoose");
 
-app.use(`/`, express.static(path.resolve(__dirname, "dist")));
+mongoose.promise = global.Promise;
+const isProduction = process.env.NODE_ENV === "production";
+
+const app = express();
+app.use(require("morgan")("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(
+  session({
+    secret: "my-tutorial",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+mongoose.connect("mongodb://localhost/passport-tutorial", {
+  useNewUrlParser: true,
+});
+mongoose.set("debug", true);
+mongoose.set("useUnifiedTopology", true);
+
+require("./models/Users");
+require("./config/passport");
+app.use(require("./routes"));
+
+if (!isProduction) {
+  // eslint-disable-next-line no-unused-vars
+  app.use((err, req, res, _) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, _) => {
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log(`Server listening on ${listener.address().port}`);
